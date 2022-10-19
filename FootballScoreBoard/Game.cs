96a26677ce4a170adee305146acae06f387b7260
Fco Javier Soler <javiersolerc@gmail.com>
2022-10-19 18:56:29 +0200

    Refactor to improve clarity and better readability of the Game class. We also remove duplicate code. The tests in green ensure the execution of the code.

--- FootballScoreBoard/Game.cs
+++ FootballScoreBoard/Game.cs
@@ -2,24 +2,16 @@
 {
     public class Game
     {
-        public string HomeTeam { get; private set; }
-        public string AwayName { get; private set; }
-        private int HomeTeamScore { get; set; }
-        private int AwayTeamScore { get; set; }
-        public string Score { get { return $"{HomeTeamScore} - {AwayTeamScore}"; } }
+        public Team HomeTeam { get; private set; }
+        public Team AwayTeam { get; private set; }
+        public string Score { get { return $"{HomeTeam.Score} - {AwayTeam.Score}"; } }
         
         public Game(string homeTeam, string awayTeam)
         {
-            if (homeTeam == null) throw new ArgumentNullException(nameof(homeTeam));
-            if (awayTeam == null) throw new ArgumentNullException(nameof(awayTeam));
-            if (homeTeam.Length == 0) throw new ArgumentException("Home team is empty");
-            if (awayTeam.Length == 0) throw new ArgumentException("Away team is empty");
-            if (homeTeam == awayTeam) throw new ArgumentException("The teams are the same and should be different");
+            HomeTeam = new Team(homeTeam);
+            AwayTeam = new Team(awayTeam);
 
-            HomeTeam = homeTeam;
-            AwayName = awayTeam;
-            HomeTeamScore = 0;
-            AwayTeamScore = 0;
+            if (HomeTeam.Name == AwayTeam.Name) throw new ArgumentException("The teams are the same and should be different");
         }
     }
 }
---
+++ FootballScoreBoard/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballScoreBoard
+{
+    public class Team
+    {
+        public string Name { get; private set; }
+        public int Score { get; set; }
+        public Team(string name)
+        {
+            if (name == null) throw new ArgumentNullException("Parameter name is null");
+            if (name.Length == 0) throw new ArgumentException("Parameter name is empty");
+
+            Name = name;
+            Score = 0;
+        }
+    }
+}
